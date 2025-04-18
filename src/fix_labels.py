18393f93588cc26e066ca10502aff062a2786e75
joannanjeri <jnjeri@iu.edu>
2025-04-18 00:57:50 -0400

    fixing labels

--- src/fix_labels.py
+++ src/fix_labels.py
@@ -7,9 +7,20 @@ label_map = {
 
 def clean_labels(file_path):
     df = pd.read_csv(file_path)
-    df['artist'] = df['artist'].map(lambda x: label_map.get(x, x)) #replace labels if in the map
-    print(f"Cleaned labels in {file_path}:")
+    
+    if 'artist' not in df.columns:
+        print(f" 'artist' column not found in {file_path}")
+        return
+    
+    #apply mapping to the 'artist' column
+    df['artist'] = df['artist'].map(lambda x: label_map.get(x, x))
 
+    #overwrite the csv file with the cleaned labels
+    df.to_csv(file_path, index=False)
+    print(f"Cleaned labels in {file_path}")
+
+#apply the cleaning function to all csv files
 for split in ['train', 'val', 'test']:
     clean_labels(f'data/{split}.csv')
+
     
